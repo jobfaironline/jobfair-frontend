58ddbba9f46d1b6c4278b234309d6fe842bc4ae1
BanhsBao <huynhbaofaker@gmail.com>
2022-01-19 01:26:47 +0700

    CAP-57 [FE] - Implement role access
    Implement role access

--- jobfair-frontend/src/router/AppRouter.js
+++ jobfair-frontend/src/router/AppRouter.js
@@ -1,1 +1,16 @@
-// handle routing
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import AttendantRouter from "./AttendantRouter";
+import StaffRouter from "./StaffRouter";
+import AdminRouter from "./AdminRouter";
+import CompanyEmployeeRouter from "./CompanyEmployee";
+import CompanyManagerRouter from "./CompanyManagerRouter";
+export default function AppRouter() {
+  return (
+    <Switch>
+      //handle public Page //handle Role Attendant Pages //handle Role Company
+      Employee Pages //handle Role Company Manager Pages //handle Role Admin
+      Pages
+    </Switch>
+  );
+}
