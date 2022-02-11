8de304761416cbdba8fccf09cacf65cec396e8b6
itshelen <68039038+HelenDao1501@users.noreply.github.com>
2022-02-11 09:27:26 +0700

    [CAP-77] Implement Change Password (#15)
    * [CAP-77] Implement Change Password
    
    Implement Change Password View + Logic
    
    * [CAP-77] Implement Change Password
    
    Deleted useless things.

--- jobfair-frontend/src/router/AppRouter.js
+++ jobfair-frontend/src/router/AppRouter.js
@@ -15,45 +15,46 @@ import ContractsPage from "../pages/ContractsPage/ContractsPage";
 import HomePage from "../pages/HomePage";
 import NavigationBar from "../components/navbar/Navbar";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
+import ChangePasswordPage from "../pages/ChangePasswordPage/ChangePasswordPage";
 const AppRouter = () => {
   return (
     <>
-    <NavigationBar/>
-    <Switch>
-      {/*init home page*/}
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
-      {/*handle public Page*/}
-      <Route path="/auth/login" exact>
-        <LoginPage />
-      </Route>
-      <Route path="/auth/register" exact>
-        <RegisterPage />
-      </Route>
-      <Route path="/faq" exact>
-        <FAQPage />
-      </Route>
-      <Route path="/JobFairPack" exact>
-        <JobFairPackPage />
-      </Route>
-      <Route path="/user" exact>
-        <UserPage />
-      </Route>
-      <Route path="/contracts" exact>
-        <ContractsPage />
-      </Route>
-      {/*handle Role Attendant Pages*/}
-
-      {/*handle Role Company Employee Pages*/}
-
-      {/*handle Role Company Manager Pages*/}
-
-      {/*handle Role Admin Pages*/}
-      <Route path="/profile" exact>
-        <ProfilePage/>
-      </Route>
-    </Switch>
+      <NavigationBar />
+      <Switch>
+        {/*init home page*/}
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        {/*handle public Page*/}
+        <Route path="/auth/login" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/auth/register" exact>
+          <RegisterPage />
+        </Route>
+        <Route path="/faq" exact>
+          <FAQPage />
+        </Route>
+        <Route path="/JobFairPack" exact>
+          <JobFairPackPage />
+        </Route>
+        <Route path="/user" exact>
+          <UserPage />
+        </Route>
+        <Route path="/contracts" exact>
+          <ContractsPage />
+        </Route>
+        {/*handle Role Attendant Pages*/}
+        {/*handle Role Company Employee Pages*/}
+        {/*handle Role Company Manager Pages*/}
+        {/*handle Role Admin Pages*/}
+        <Route path="/profile" exact>
+          <ProfilePage />
+        </Route>
+        <Route path="/accounts/changepassword" exact>
+          <ChangePasswordPage />
+        </Route>
+      </Switch>
     </>
   );
 };
