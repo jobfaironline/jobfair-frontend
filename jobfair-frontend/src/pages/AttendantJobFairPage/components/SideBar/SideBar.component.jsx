0a789370540ea2674dd5943ee4bc0ca2838dccd4
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-03-03 02:10:08 +0700

    Implement UI Attendant Job Fair Page
    Fix Sidebar logic

--- jobfair-frontend/src/pages/AttendantJobFairPage/components/SideBar/SideBar.component.jsx
+++ jobfair-frontend/src/pages/AttendantJobFairPage/components/SideBar/SideBar.component.jsx
@@ -13,14 +13,13 @@ const SideBar = () => {
   The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
   Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`)
   const handleOpenDetail = status => {
+    console.log(status)
     setIsShow(status)
   }
-  const handleOnChange = key => {
-    setCurrentKey(key)
-  }
+
   return (
     <div className={styles.container} defaultActiveKey={1} accessKey={currentKey}>
-      <Tabs tabPosition="left" onChange={handleOnChange}>
+      <Tabs tabPosition="left">
         <TabPane
           tab={
             <span>
@@ -34,20 +33,24 @@ const SideBar = () => {
           key="1"
           disabled
         ></TabPane>
-        <TabPane tab="Tab 1" key="2">
-          <div className={styles.aboutCompany}>
-            <Typography variant="button" disabled={isShow}>
-              About Company
-            </Typography>
-            <CompanyInfor companyInfor={companyInfor} />
-          </div>
-        </TabPane>
-        <TabPane tab="Tab 2" key="3" disabled={isShow}>
-          <Typography variant="button">Detail one</Typography>
-        </TabPane>
-        <TabPane tab="Tab 3" key="4" disabled={isShow}>
-          <Typography variant="button">Detail two</Typography>
-        </TabPane>
+        {isShow ? (
+          <>
+            <TabPane tab="Tab 1" key="2">
+              <div className={styles.aboutCompany}>
+                <Typography variant="button">About Company</Typography>
+                <CompanyInfor companyInfor={companyInfor} />
+              </div>
+            </TabPane>
+            <TabPane tab="Tab 2" key="3">
+              <Typography variant="button">Detail one</Typography>
+            </TabPane>
+            <TabPane tab="Tab 3" key="4">
+              <Typography variant="button">Detail two</Typography>
+            </TabPane>
+          </>
+        ) : (
+          <></>
+        )}
       </Tabs>
     </div>
   )
