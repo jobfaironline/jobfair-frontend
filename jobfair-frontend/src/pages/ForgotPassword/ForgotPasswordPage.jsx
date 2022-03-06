5d1b27e40c5c6635eb6eee594f39c6d7102e20e9
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-03-06 17:15:57 +0700

    [CAP-76] - Implement forgot password (#51)
    * [CAP-76] - Implement forgot password
    
     - Implement forgot password
    
    * Implement forgot password
    
    Fix Forgot password routing
    
    * [CAP-76] Implement Reset password
    
    * [CAP-76] Fix message
    
    * [CAP-76] Implement forgot password
    
    * [CAP-76] Implement Forgot Password Page
    
    * [CAP-76] Implement forget password

---
+++ jobfair-frontend/src/pages/ForgotPassword/ForgotPasswordPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ForgotPasswordContainer from '../../containers/ForgotPassword/ForgotPassword.container'
+const ForgotPasswordPage = () => {
+  return (
+    <div className="page">
+      <ForgotPasswordContainer />
+    </div>
+  )
+}
+export default ForgotPasswordPage
--- jobfair-frontend/src/services/account-controller/AccountControllerService.js
+++ jobfair-frontend/src/services/account-controller/AccountControllerService.js
@@ -1,7 +1,9 @@
 import { CallAPI } from '../axiosBase'
 import {
   ENDPOINT_CHANGE_PASSWORD,
-  ENDPOINT_GET_ACCOUNT_BY_ID
+  ENDPOINT_GET_ACCOUNT_BY_ID,
+  ENDPOINT_FORGOT_PASSWORD
 } from '../../constants/Endpoints/account-controller/AccountControllerEndpoint'
 export const changePasswordAPI = data => CallAPI(ENDPOINT_CHANGE_PASSWORD, 'POST', data)
 export const getAccountByIdAPI = id => CallAPI(`${ENDPOINT_GET_ACCOUNT_BY_ID}/${id}`, 'GET')
+export const forgotPasswordAPI = data => CallAPI(ENDPOINT_FORGOT_PASSWORD, 'POST', data)
