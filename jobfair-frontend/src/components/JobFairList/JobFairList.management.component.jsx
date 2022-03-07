748d04395d0395d06a4c454111fad7e819e12f7b
Tien T. Truong <tientt1938@gmail.com>
2022-03-07 15:42:33 +0700

    [CAP-202] revamp status for company job fair

--- jobfair-frontend/src/components/JobFairList/JobFairList.management.component.jsx
+++ jobfair-frontend/src/components/JobFairList/JobFairList.management.component.jsx
@@ -3,12 +3,73 @@ import { Button, Divider, List, Select, Skeleton, Space, Tag, Tooltip, Typograph
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { JOB_FAIR_PLAN_COMPANY_STATUS } from '../../constants/JobFairConst'
 import { PATH } from '../../constants/Paths/Path'
+import {COMPANY_JOB_FAIR_STATUS} from "../../constants/CompanyJobFairStatus";
 
 const JobFairListManagementComponent = props => {
   const { Title, Paragraph, Text, Link } = Typography
   const { Option } = Select
 
   const { data, handleRedirect, loadMoreData, handleFilterByStatus, searchResult, getCompanyBoothId } = props
+
+  const generateButton = (status) => {
+      switch (status){
+          case COMPANY_JOB_FAIR_STATUS.REGISTRABLE:
+              return (
+                  <Tooltip title="This event is open. Register now" color="green">
+                      <Button
+                          type="primary"
+                          onClick={() => handleRedirect(`${PATH.JOB_FAIR_REGISTRATION_PAGE}${item.id}`)}
+                      >
+                          Register now
+                      </Button>
+                  </Tooltip>
+              )
+          case COMPANY_JOB_FAIR_STATUS.SUBMITTED:
+              return (
+                  <Tooltip title="You registration is still in progress. Please wait!" color="gold">
+                      <Button type="primary">Is evaluating</Button>
+                  </Tooltip>
+              )
+          case COMPANY_JOB_FAIR_STATUS.APPROVE:
+              return (
+                  <Tooltip title="Your registration was approved!" color="gold">
+                      <Button type="primary">Approved</Button>
+                  </Tooltip>
+              )
+          case COMPANY_JOB_FAIR_STATUS.UNAVAILABLE:
+              return (
+                  <Tooltip title="This event was delayed. Please comeback later." color="red">
+                      <Button type="primary" disabled>
+                          Suspended
+                      </Button>
+                  </Tooltip>
+              )
+          case COMPANY_JOB_FAIR_STATUS.DECORATE_BOOTH:
+              return (
+                  <Tooltip title="You chose a booth in this event. Decorate it now" color="geekblue">
+                      <Button type="primary" onClick={() => getCompanyBoothId(item.id)}>
+                          Decorate booth
+                      </Button>
+                  </Tooltip>
+              )
+          case COMPANY_JOB_FAIR_STATUS.CHOOSE_BOOTH:
+              return (
+                  <Tooltip title="You registration has been approved. Now you can choose booth" color="blue">
+                      <Button type="primary" onClick={() => handleRedirect(`${PATH.CHOOSE_BOOTH_PATH}${item.id}`)}>
+                          Choose booth
+                      </Button>
+                  </Tooltip>
+              )
+          default:
+              return (
+                  <Tooltip title="Other status" color="blue">
+                      <Button type="primary" onClick={() => {}}>
+                          {status}
+                      </Button>
+                  </Tooltip>
+              )
+      }
+  }
 
   return (
     <div
@@ -52,47 +113,7 @@ const JobFairListManagementComponent = props => {
                   <Button type="link" onClick={() => {}} style={{ padding: '0.2rem 0', border: '0' }}>
                     More details
                   </Button>
-                  {item.status === 'REGISTRABLE' ? (
-                    <Tooltip title="This event is open. Register now" color="green">
-                      <Button
-                        type="primary"
-                        onClick={() => handleRedirect(`${PATH.JOB_FAIR_REGISTRATION_PAGE}${item.id}`)}
-                      >
-                        Register now
-                      </Button>
-                    </Tooltip>
-                  ) : null}
-                  {item.status === 'SUBMITTED' ? (
-                    <Tooltip title="You registration is still in progress. Please wait!" color="gold">
-                      <Button type="primary">Is evaluating</Button>
-                    </Tooltip>
-                  ) : null}
-                  {item.status === 'APPROVE' ? (
-                    <Tooltip title="Your registration was approved!" color="gold">
-                      <Button type="primary">Approved</Button>
-                    </Tooltip>
-                  ) : null}
-                  {item.status === 'UNAVAILABLE' ? (
-                    <Tooltip title="This event was delayed. Please comeback later." color="red">
-                      <Button type="primary" disabled>
-                        Suspended
-                      </Button>
-                    </Tooltip>
-                  ) : null}
-                  {item.status === 'DECORATE_BOOTH' ? (
-                    <Tooltip title="You chose a booth in this event. Decorate it now" color="geekblue">
-                      <Button type="primary" onClick={() => getCompanyBoothId(item.id)}>
-                        Decorate booth
-                      </Button>
-                    </Tooltip>
-                  ) : null}
-                  {item.status === 'CHOOSE_BOOTH' ? (
-                    <Tooltip title="You registration has been approved. Now you can choose booth" color="blue">
-                      <Button type="primary" onClick={() => handleRedirect(`${PATH.CHOOSE_BOOTH_PATH}${item.id}`)}>
-                        Choose booth
-                      </Button>
-                    </Tooltip>
-                  ) : null}
+                    {generateButton(item.status)}
                 </Space>
               ]}
             >
@@ -100,12 +121,6 @@ const JobFairListManagementComponent = props => {
                 title={
                   <div display="flex">
                     <h2 style={{ marginBottom: '0.2rem' }}>{`Name: ${item['description']}`}</h2>
-                    {/*{item.status === 'UNAVAILABLE'? <Tag color="red">{item.status}</Tag> : null}*/}
-                    {/*{item.status === 'REGISTRABLE' ? <Tag color="green">{item.status}</Tag> : null}*/}
-                    {/*{item.status === 'SUBMITTED' ? <Tag color="gold">{item.status}</Tag> : null}*/}
-                    {/*{item.status === 'CHOOSE_BOOTH' ? <Tag color="blue">{item.status}</Tag> : null}*/}
-                    {/*{item.status === 'DECORATE_BOOTH' ? <Tag color="geekblue">{item.status}</Tag> : null}*/}
-                    {/*{item.status === 'APPROVE' ? <Tag color="geekblue">{item.status}</Tag> : null}*/}
                   </div>
                 }
                 description={
