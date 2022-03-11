1de552aba475badb40633c87850963a7728233f6
BanhsBao <huynhbaofaker@gmail.com>
2022-03-11 20:49:20 +0700

    [CAP-233] - Implement Policy Page

--- jobfair-frontend/src/pages/FAQPage/FAQPage.jsx
+++ jobfair-frontend/src/pages/FAQPage/FAQPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import PolicyComponent from '../../components/Policy/Policy.component'
 
 const FAQPage = () => {
   return (
-    <div>
-      <h2>FAQ Page</h2>
+    <div
+      className="page"
+      style={{
+        padding: '10rem',
+        position: 'absolute',
+        left: '50%',
+        transform: 'translate(-50%, -10%)',
+        marginTop: '20rem'
+      }}
+    >
+      <PolicyComponent />
     </div>
   )
 }
