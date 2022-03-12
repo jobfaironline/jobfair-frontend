c0d8e4e27e7a280051879d76980d88ac8e9cb594
BanhsBao <huynhbaofaker@gmail.com>
2022-03-13 01:26:27 +0700

    Change flow request

--- jobfair-frontend/src/pages/FAQPage/FAQPage.jsx
+++ jobfair-frontend/src/pages/FAQPage/FAQPage.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import PolicyComponent from '../../components/Policy/Policy.component'
 
 const FAQPage = () => {
   return (
-    <div
-      className="page"
-      style={{
-        padding: '10rem',
-        position: 'absolute',
-        left: '50%',
-        transform: 'translate(-50%, -10%)',
-        marginTop: '20rem'
-      }}
-    >
-      <PolicyComponent />
+    <div>
+      <h2>FAQ Page</h2>
     </div>
   )
 }
