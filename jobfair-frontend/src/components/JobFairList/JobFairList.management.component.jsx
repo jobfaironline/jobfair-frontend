180b1cbf5b38f0196a65e3801e8d405e9b656ccc
BanhsBao <huynhbaofaker@gmail.com>
2022-03-15 12:57:08 +0700

    [FIX] Jobfair list responsive

--- jobfair-frontend/src/components/JobFairList/JobFairList.management.component.jsx
+++ jobfair-frontend/src/components/JobFairList/JobFairList.management.component.jsx
@@ -1,24 +1,8 @@
 import React from 'react'
-import {
-  Button,
-  Divider,
-  List,
-  Select,
-  Skeleton,
-  Space,
-  Tag,
-  Tooltip,
-  Typography,
-  Carousel,
-  Image,
-  Empty,
-  Card,
-  Badge
-} from 'antd'
+import { Divider, List, Select, Skeleton, Space, Tag, Typography, Carousel, Card, Badge, Row, Col } from 'antd'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import ShowMoreText from 'react-show-more-text'
 import { JOB_FAIR_FOR_ATTENDANT_STATUS, JOB_FAIR_PLAN_COMPANY_STATUS } from '../../constants/JobFairConst'
-import { PATH } from '../../constants/Paths/Path'
 import { COMPANY_JOB_FAIR_STATUS } from '../../constants/CompanyJobFairStatus'
 import CompanyJobFairActionButton from './CompanyJobFairActionButton.component'
 import JobFairListManagementImageComponent from './components/JobFairList.management.image.component'
@@ -124,133 +108,130 @@ const JobFairListManagementComponent = props => {
                     bordered={true}
                     style={{
                       borderRadius: '8px',
-                      marginRight: '24px',
-                      marginLeft: '24px',
                       boxShadow: '3px 6px 22px 3px rgba(208, 216, 243, 0.6)',
                       width: '98%'
                     }}
                   >
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                      <div style={{ display: 'flex' }}>
+                    <Row justify="space-between">
+                      <Col span={6}>
+                        <Badge.Ribbon
+                          text={
+                            <Text strong style={{ color: 'white', fontSize: '1.3rem' }}>
+                              17 Aug
+                            </Text>
+                          }
+                          style={{ height: '1.7rem' }}
+                        >
+                          <Carousel autoplay>
+                            {listImage.map(image => (
+                              <div style={contentStyle}>
+                                <JobFairListManagementImageComponent key={image.id} urlImage={image.urlImage} />
+                              </div>
+                            ))}
+                          </Carousel>
+                        </Badge.Ribbon>
+                      </Col>
+                      <Col span={14}>
                         <div>
-                          <Badge.Ribbon
-                            text={
-                              <Text strong style={{ color: 'white', fontSize: '20px' }}>
-                                17 Aug
-                              </Text>
-                            }
-                            style={{ height: '28px' }}
-                          >
-                            <Carousel autoplay style={{ width: '450px' }}>
-                              {listImage.map(image => (
-                                <div style={contentStyle}>
-                                  <JobFairListManagementImageComponent key={image.id} urlImage={image.urlImage} />
-                                </div>
-                              ))}
-                            </Carousel>
-                          </Badge.Ribbon>
+                          <Title level={2}>{`${item['description']}`}</Title>
                         </div>
-                        <div style={{ marginLeft: '5rem', width: '1000px' }}>
-                          <div>
-                            <Title level={2}>{`${item['description']}`}</Title>
-                          </div>
-                          <div style={{ display: 'flex', justifyContent: 'space-between', width: '700px' }}>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', width: '200px' }}>
-                              <div>
-                                <Title level={5} type="secondary">
-                                  Start Date
-                                </Title>
-
-                                <Tag color="cyan">
-                                  <Text strong>25/10/2022</Text>
-                                </Tag>
-                              </div>
-                              <div>
-                                <Title level={5} type="secondary">
-                                  End Date
-                                </Title>
-                                <Tag color="blue">
-                                  <Text strong>25/10/2022</Text>
-                                </Tag>
-                              </div>
-                            </div>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
-                              <div>
+                        <Row justify="space-between">
+                          <Row justify="space-between" gutter={32}>
+                            <Col span={12}>
+                              <Title level={5} type="secondary">
+                                Start Date
+                              </Title>
+                              <Tag color="cyan">
+                                <Text strong>25/10/2022</Text>
+                              </Tag>
+                            </Col>
+                            <Col span={12}>
+                              <Title level={5} type="secondary">
+                                End Date
+                              </Title>
+                              <Tag color="blue">
+                                <Text strong>25/10/2022</Text>
+                              </Tag>
+                            </Col>
+                          </Row>
+                          <Col span={7}>
+                            <Row justify='space-between'>
+                              <Col span={14}>
                                 <Title level={5} type="secondary">
                                   Location
                                 </Title>
                                 <Text strong>Some where in saigon</Text>
-                              </div>
-                              <div>
+                              </Col>
+                              <Col span={8}>
                                 <Title level={5} type="secondary">
                                   Register time
                                 </Title>
                                 <Tag color="green">
                                   <Text strong>25/10/2022</Text>
                                 </Tag>
-                              </div>
-                            </div>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                        <div style={{ marginTop: '2rem' }}>
+                          <Title level={5} type="secondary">
+                            Description
+                          </Title>
+                          <Text strong>
+                            <ShowMoreText
+                              lines={3}
+                              more=".."
+                              less=".."
+                              className="content-css"
+                              anchorClass="my-anchor-css-class"
+                              expanded={false}
+                              width={1000}
+                              truncatedEndingComponent={'.'}
+                            >
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                              been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+                              a galley of type and scrambled it to make a type specimen book. It has survived not only
+                              five centuries, but also the leap into electronic typesetting, remaining essentially
+                              unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+                              Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                              PageMaker including versions of Lorem Ipsum.
+                            </ShowMoreText>
+                          </Text>
+                        </div>
+                      </Col>
+                      <Col span={3}>
+                        <div>
+                          <div style={{ float: 'right' }}>
+                            {item.status == COMPANY_JOB_FAIR_STATUS.REJECT ? (
+                              <Space>
+                                <Typography style={{ color: '#DD0000' }}>
+                                  Your recent registration has been rejected, please try again!
+                                </Typography>
+                              </Space>
+                            ) : null}
+                            {isAuthUser && listRole.includes(ATTENDANT) ? (
+                              <Tag color="lime">
+                                <UserOutlined /> 300
+                              </Tag>
+                            ) : null}
+                            {(isAuthUser && listRole.includes(COMPANY_EMPLOYEE)) ||
+                            (isAuthUser && listRole.includes(COMPANY_MANAGER)) ? (
+                              <Tag color="geekblue">
+                                <BankTwoTone /> 300
+                              </Tag>
+                            ) : null}
+                            <InfoCircleTwoTone />
                           </div>
-                          <div style={{ marginTop: '2rem' }}>
-                            <Title level={5} type="secondary">
-                              Description
-                            </Title>
-                            <Text strong>
-                              <ShowMoreText
-                                lines={3}
-                                more=".."
-                                less=".."
-                                className="content-css"
-                                anchorClass="my-anchor-css-class"
-                                expanded={false}
-                                width={1000}
-                                truncatedEndingComponent={'.'}
-                              >
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                                has been the industry's standard dummy text ever since the 1500s, when an unknown
-                                printer took a galley of type and scrambled it to make a type specimen book. It has
-                                survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged. It was popularised in the 1960s with the release of
-                                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                              </ShowMoreText>
-                            </Text>
+                          <div style={{ position: 'absolute', bottom: '1rem' }}>
+                            <CompanyJobFairActionButton
+                              getCompanyBoothId={getCompanyBoothId}
+                              item={item}
+                              handleRedirect={handleRedirect}
+                            />
                           </div>
                         </div>
-                      </div>
-                      <div>
-                        {item.status == COMPANY_JOB_FAIR_STATUS.REJECT ? (
-                          <Space>
-                            <Typography style={{ color: '#DD0000' }}>
-                              Your recent registration has been rejected, please try again!
-                            </Typography>
-                          </Space>
-                        ) : null}
-                        <div
-                          style={{ display: 'flex', justifyContent: 'space-between', fontSize: '1.2rem', gap: '10px' }}
-                        >
-                          {isAuthUser && listRole.includes(ATTENDANT) ? (
-                            <Tag color="lime">
-                              <UserOutlined /> 300
-                            </Tag>
-                          ) : null}
-                          {(isAuthUser && listRole.includes(COMPANY_EMPLOYEE)) ||
-                          (isAuthUser && listRole.includes(COMPANY_MANAGER)) ? (
-                            <Tag color="geekblue">
-                              <BankTwoTone /> 300
-                            </Tag>
-                          ) : null}
-                          <InfoCircleTwoTone />
-                        </div>
-                        <div style={{ position: 'absolute', bottom: '2rem', marginLeft: '-5rem' }}>
-                          <CompanyJobFairActionButton
-                            getCompanyBoothId={getCompanyBoothId}
-                            item={item}
-                            handleRedirect={handleRedirect}
-                          />
-                        </div>
-                      </div>
-                    </div>
+                      </Col>
+                    </Row>
                   </Card>
                 }
               />
