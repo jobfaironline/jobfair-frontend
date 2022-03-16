ff6770bfb074fb2c632115ff9c86cd6ca3bcd827
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-03-16 16:53:09 +0700

    [CAP-235] Style Anchor Page

--- jobfair-frontend/src/components/Anchor/Achor.component.jsx
+++ jobfair-frontend/src/components/Anchor/Achor.component.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from 'react'
 import { Anchor, Menu, Button } from 'antd'
 import { MenuUnfoldOutlined, MenuFoldOutlined, PieChartOutlined } from '@ant-design/icons'
-import Drawer from '@mui/material/Drawer'
 const { SubMenu } = Menu
 const { Link } = Anchor
 const AnchorComponent = props => {
   const { listData, href, title } = props
-  const [isOpen, setIsOpen] = useState({ right: false })
+  const [isHide, setIsHide] = useState(false)
   const handleOnOpenMenu = status => {
-    setIsOpen(status)
+    console.log(status)
+    setIsHide(status)
   }
   return (
     <>
-      <Button type="primary" onClick={() => handleOnOpenMenu(true)} style={{ marginBottom: 16 }}>
-        {isOpen ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      <Button type="primary" style={{ marginBottom: 10 }}>
+        <div style={{ fontSize: '15px' }}>
+          {isHide ? (
+            <MenuUnfoldOutlined onClick={() => handleOnOpenMenu(false)} />
+          ) : (
+            <MenuFoldOutlined onClick={() => handleOnOpenMenu(true)} />
+          )}
+        </div>
       </Button>
 
-      <Drawer anchor={'left'} open={isOpen} onClose={() => handleOnOpenMenu(false)}>
+      <div
+        style={{
+          borderRadius: '0.5rem 0.5rem 0.5rem 0.5rem',
+          position: 'absolute',
+          boxShadow: '0 8px 8px -4px lightblue',
+          width: '10rem',
+          maxWidth: '15rem'
+        }}
+        hidden={isHide}
+      >
         {
           <div style={{ padding: '10px', marginTop: '10px' }}>
             <Anchor targetOffset={300} onClick={e => e.preventDefault()}>
@@ -27,7 +42,7 @@ const AnchorComponent = props => {
             </Anchor>
           </div>
         }
-      </Drawer>
+      </div>
     </>
   )
 }
