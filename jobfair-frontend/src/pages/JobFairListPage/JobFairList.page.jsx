6f328afb26d3bd9fcc4da2cfab303954ada780bb
ZERO-94 <70008423+ZERO-94@users.noreply.github.com>
2022-03-16 21:33:44 +0700

    Cap 271 - Styling job fair table of admin and add username (#94)
    * fix styling admin job fair table
    
    * add username
    
    Co-authored-by: ANH_KTV <anhktv@fundiin.vn>

--- jobfair-frontend/src/pages/JobFairListPage/JobFairList.page.jsx
+++ jobfair-frontend/src/pages/JobFairListPage/JobFairList.page.jsx
@@ -1,34 +1,36 @@
 import React from 'react'
-import {Divider, Tabs, Typography} from 'antd'
-import {useSelector} from 'react-redux'
-import JobFairOccurredContainer from "../../containers/JobFairList/admin/JobFairOccurred.container";
-import JobFairHappeningContainer from "../../containers/JobFairList/admin/JobFairHappening.container";
-import JobFairIncomingContainer from "../../containers/JobFairList/admin/JobFairIncoming.container";
+import { Divider, Tabs, Typography } from 'antd'
+import { useSelector } from 'react-redux'
+import JobFairOccurredContainer from '../../containers/JobFairList/admin/JobFairOccurred.container'
+import JobFairHappeningContainer from '../../containers/JobFairList/admin/JobFairHappening.container'
+import JobFairIncomingContainer from '../../containers/JobFairList/admin/JobFairIncoming.container'
+import './JobFairListPage.styles.scss'
 
-
-const {TabPane} = Tabs;
-const {Text} = Typography;
+const { TabPane } = Tabs
+const { Text } = Typography
 const JobFairListPage = () => {
-    const role = useSelector(state => state.authentication?.user?.roles)
+  const role = useSelector(state => state.authentication?.user?.roles)
 
-    return (
-        <div className="page" style={{marginTop: 80}}>
-            <Divider>
-                <Text strong>Job fair list</Text>
-            </Divider>
-            <Tabs defaultActiveKey="2" centered>
-                <TabPane tab="Taken place" key="1">
-                    <JobFairOccurredContainer/>
-                </TabPane>
-                <TabPane tab="Happening" key="2">
-                    <JobFairHappeningContainer/>
-                </TabPane>
-                <TabPane tab="Coming soon" key="3">
-                    <JobFairIncomingContainer/>
-                </TabPane>
-            </Tabs>
-        </div>
-    )
+  return (
+    <div className="page jobfair-list-page">
+      <div style={{ padding: '2rem 0' }}>
+        <Divider>
+          <Typography.Title level={2}>Job fair list</Typography.Title>
+        </Divider>
+        <Tabs defaultActiveKey="2" centered>
+          <TabPane tab="Taken place" key="1">
+            <JobFairOccurredContainer />
+          </TabPane>
+          <TabPane tab="Happening" key="2">
+            <JobFairHappeningContainer />
+          </TabPane>
+          <TabPane tab="Coming soon" key="3">
+            <JobFairIncomingContainer />
+          </TabPane>
+        </Tabs>
+      </div>
+    </div>
+  )
 }
 
 export default JobFairListPage
