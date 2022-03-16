9c669f94cd75ac263e4c6b7d9375347c33236978
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-03-16 10:37:09 +0700

    Fix bug

--- jobfair-frontend/src/components/create-job-position-form/CreateJobPositionForm.jsx
+++ jobfair-frontend/src/components/create-job-position-form/CreateJobPositionForm.jsx
@@ -40,8 +40,8 @@ const CreateJobPositionForm = props => {
   const [resultNameSuggested, setResultNameSuggested] = useState([])
   const [resultEmailSuggested, setResultEmailSuggested] = useState([])
   useEffect(() => {
-    setListContactPersonSuggestion(location.state.listContactPersonSuggestion)
-    setListEmailSuggestion(location.state.listEmailSuggestion)
+    setListContactPersonSuggestion(location.state?.listContactPersonSuggestion)
+    setListEmailSuggestion(location.state?.listEmailSuggestion)
   }, [location])
   const handleAutoCompleteContactPerson = value => {
     let res = []
@@ -72,14 +72,15 @@ const CreateJobPositionForm = props => {
 
   return (
     <div style={{ width: '80%' }}>
-      <Form onFinish={onFinish} form={form} {...formItemLayout}>
-        <Card title={`Create job position application`} headStyle={{ fontWeight: 700, fontSize: 24 }}>
+      <Card title={`Create job position application`} style={{ width: '70%', margin: '3rem auto' }}>
+        <Form onFinish={onFinish} form={form} {...formItemLayout} layout="vertical" labelCol={21} wrapperCol={21}>
           <Form.Item
             label="Job title"
             name="title"
             required
             tooltip="This is required"
             rules={JobPositionValidation.title}
+            style={{ width: '95%', marginLeft: '1rem', marginRight: '1rem' }}
           >
             <Input placeholder="Job title" />
           </Form.Item>
@@ -89,6 +90,7 @@ const CreateJobPositionForm = props => {
             tooltip="This is required"
             name="level"
             rules={JobPositionValidation.jobLevel}
+            style={{ display: 'inline-block', width: '26%', marginRight: '1rem', marginLeft: '1rem' }}
           >
             <Select
               showSearch
@@ -107,6 +109,7 @@ const CreateJobPositionForm = props => {
             tooltip="This is required"
             rules={JobPositionValidation.jobType}
             name="jobType"
+            style={{ display: 'inline-block', width: '30%', marginLeft: '1rem', marginRight: '1rem' }}
           >
             <Select
               showSearch
@@ -120,11 +123,68 @@ const CreateJobPositionForm = props => {
             </Select>
           </Form.Item>
           <Form.Item
-            label="Job category"
+            label="Prefer language"
+            name={'preferredLanguage'}
+            required
+            rules={JobPositionValidation.language}
+            style={{ display: 'inline-block', width: '30%', marginLeft: '1.25rem' }}
+          >
+            <Select
+              showSearch
+              placeholder="Search to Select"
+              optionFilterProp="children"
+              filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+              filterSort={(optionA, optionB) =>
+                optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
+              }
+            >
+              {LanguageConst.map(item => (
+                <Option value={item.value}>{item.value}</Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Skill tags"
+            required
+            tooltip="This is required"
+            rules={JobPositionValidation.skillTags}
+            name="skillTagIds"
+            style={{ display: 'inline-block', width: '45%', marginLeft: '1rem', marginRight: '1.25rem' }}
+          >
+            <Select
+              mode="multiple"
+              onChange={value => {
+                //value is a array
+                if (value.length > NUM_OF_SKILL_TAGS) {
+                  value.pop()
+                }
+                setTotalSkillTags(value.length)
+              }}
+              onSearch={value => {}}
+              dropdownRender={menu => (
+                <>
+                  {menu}
+                  <Divider style={{ margin: '8px 0' }} />
+                  <Text type={totalSkillTags > 3 ? 'danger' : 'success'}>
+                    {totalSkillTags > 5
+                      ? null
+                      : `You can select ${NUM_OF_SKILL_TAGS} items only. (${NUM_OF_SKILL_TAGS - totalSkillTags} left)`}
+                  </Text>
+                </>
+              )}
+            >
+              {SkillTagsConst.map(item => (
+                <Option value={item.id}>{item.name}</Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Category tag"
             required
             tooltip="This is required"
             rules={JobPositionValidation.jobCategory}
             name="subCategoryIds"
+            style={{ display: 'inline-block', width: '45%', marginLeft: '1.25rem', marginRight: '1rem' }}
           >
             <Select
               mode="multiple"
@@ -158,63 +218,12 @@ const CreateJobPositionForm = props => {
             </Select>
           </Form.Item>
           <Form.Item
-            label="Description"
-            required
-            tooltip="This is required"
-            rules={JobPositionValidation.description}
-            name="description"
-          >
-            <TextArea placeholder="Description" showCount maxLength={3000} autoSize={{ minRows: 5 }} />
-          </Form.Item>
-          <Form.Item
-            label="Requirements"
-            required
-            tooltip="This is required"
-            rules={JobPositionValidation.requirements}
-            name="requirements"
-          >
-            <TextArea placeholder="Requirements" showCount maxLength={3000} autoSize={{ minRows: 5 }} />
-          </Form.Item>
-          <Form.Item
-            label="Skill tags"
-            required
-            tooltip="This is required"
-            rules={JobPositionValidation.skillTags}
-            name="skillTagIds"
-          >
-            <Select
-              mode="multiple"
-              onChange={value => {
-                //value is a array
-                if (value.length > NUM_OF_SKILL_TAGS) {
-                  value.pop()
-                }
-                setTotalSkillTags(value.length)
-              }}
-              onSearch={value => {}}
-              dropdownRender={menu => (
-                <>
-                  {menu}
-                  <Divider style={{ margin: '8px 0' }} />
-                  <Text type={totalSkillTags > 3 ? 'danger' : 'success'}>
-                    {totalSkillTags > 5
-                      ? null
-                      : `You can select ${NUM_OF_SKILL_TAGS} items only. (${NUM_OF_SKILL_TAGS - totalSkillTags} left)`}
-                  </Text>
-                </>
-              )}
-            >
-              {SkillTagsConst.map(item => (
-                <Option value={item.id}>{item.name}</Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
             label="Contact person"
             required
             tooltip="This is required"
             rules={JobPositionValidation.contactPerson}
             name="contactPersonName"
+            style={{ display: 'inline-block', width: '45%', marginLeft: '1rem', marginRight: '1rem' }}
           >
             <AutoComplete
               style={{
@@ -230,36 +239,13 @@ const CreateJobPositionForm = props => {
               <Input placeholder="Contact person name" />
             </AutoComplete>
           </Form.Item>
-          <Form.Item label="Language" name={'preferredLanguage'} required rules={JobPositionValidation.language}>
-            <Select
-              showSearch
-              placeholder="Search to Select"
-              optionFilterProp="children"
-              filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-              filterSort={(optionA, optionB) =>
-                optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
-              }
-            >
-              {LanguageConst.map(item => (
-                <Option value={item.value}>{item.value}</Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label="Location for applications"
-            required
-            tooltip="This is required"
-            rules={JobPositionValidation.contactPerson}
-            name="locationId"
-          >
-            <Input placeholder="Location" />
-          </Form.Item>
           <Form.Item
             label="Email for applications"
             required
             tooltip="This is required"
             rules={JobPositionValidation.email}
             name="contactEmail"
+            style={{ display: 'inline-block', width: '45%', marginLeft: '1rem', marginRight: '1rem' }}
           >
             <AutoComplete
               style={{
@@ -275,6 +261,36 @@ const CreateJobPositionForm = props => {
               <Input placeholder="Email for receiving applications" />
             </AutoComplete>
           </Form.Item>
+          <Form.Item
+            label="Location for applications"
+            required
+            tooltip="This is required"
+            rules={JobPositionValidation.contactPerson}
+            name="locationId"
+            style={{ display: 'inline-block', width: '96%', marginLeft: '1rem', marginRight: '1rem' }}
+          >
+            <Input placeholder="Location" />
+          </Form.Item>
+          <Form.Item
+            label="Description"
+            required
+            tooltip="This is required"
+            rules={JobPositionValidation.description}
+            name="description"
+            style={{ marginLeft: '1rem', width: '96%' }}
+          >
+            <TextArea placeholder="Description" showCount maxLength={3000} autoSize={{ minRows: 5 }} />
+          </Form.Item>
+          <Form.Item
+            label="Requirements"
+            required
+            tooltip="This is required"
+            rules={JobPositionValidation.requirements}
+            name="requirements"
+            style={{ marginLeft: '1rem', width: '96%' }}
+          >
+            <TextArea placeholder="Requirements" showCount maxLength={3000} autoSize={{ minRows: 5 }} />
+          </Form.Item>
           <Form.Item style={{ display: 'flex', justifyContent: 'end' }}>
             <Space style={{ display: 'flex', justifyContent: 'end' }}>
               <Button type="primary" htmlType="submit" style={{ margin: '0 3rem', width: '7rem' }}>
@@ -282,8 +298,8 @@ const CreateJobPositionForm = props => {
               </Button>
             </Space>
           </Form.Item>
-        </Card>
-      </Form>
+        </Form>
+      </Card>
     </div>
   )
 }
