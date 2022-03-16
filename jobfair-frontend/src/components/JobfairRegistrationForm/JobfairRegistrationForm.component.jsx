d9a195c7529d28120c2b50fb8f3bbb959aaa3ebc
Son Pham Cao <62231837+CaoSon1909@users.noreply.github.com>
2022-03-16 23:25:16 +0700

    hot_fix: form component (#95)

--- jobfair-frontend/src/components/JobfairRegistrationForm/JobfairRegistrationForm.component.jsx
+++ jobfair-frontend/src/components/JobfairRegistrationForm/JobfairRegistrationForm.component.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Button, Form, notification, Typography} from 'antd'
 import { CompanyProfileValidation } from '../../validate/CompanyProfileValidation'
 import TextArea from 'antd/es/input/TextArea'
@@ -8,7 +8,27 @@ import {
   getLatestCompanyRegistration
 } from "../../services/company-registration-controller/CompanyRegistrationControllerService";
 
+const {Text} = Typography
+
 const JobfairRegistrationForm = ({ form , jobFairId}) => {
+
+  const [adminMessage, setAdminMessage] = useState('')
+  const fetchData = async () => {
+    if (jobFairId !== undefined) {
+      getLatestCompanyRegistration(jobFairId)
+        .then(res => {
+          console.log(res.data)
+          setAdminMessage(res.data.adminMessage)
+        })
+        .catch(err => {
+          console.log(err.response.data)
+        })
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   {/* TODO: fetch latest company registration*/}
   // const fetchData = async () => {
@@ -40,25 +60,38 @@ const JobfairRegistrationForm = ({ form , jobFairId}) => {
   //   fetchData()
   // }, [])
   return (
-    <Form
-      form={form}
-      layout="vertical"
-      requiredMark="required"
-      autoComplete="off"
-      scrollToFirstError
-      initialValues={{ description: undefined, jobPositions: [] }} //will go
-    >
-      <Form.Item
-        label="Registration description"
-        required
-        tooltip="This description will be shown during the jobfair"
-        rules={CompanyProfileValidation.description}
-        name="description"
+    <>
+      {adminMessage.length !== 0 ?
+        (
+          <div style={{marginBottom: 20}}>
+            <Text strong>Admin request change content: </Text>
+            <Text>
+              {adminMessage}
+            </Text>
+          </div>
+        ) : null}
+
+      <Form
+        form={form}
+        layout="vertical"
+        requiredMark="required"
+        autoComplete="off"
+        scrollToFirstError
+        initialValues={{ description: undefined, jobPositions: [] }} //will go
       >
-        <TextArea autoSize={{ minRows: 5 }} showCount maxLength={3000} placeholder="Registration description" />
-      </Form.Item>
-      <PickJobPositionFormContainer form={form}/>
-    </Form>
+        <Form.Item
+          label="Registration description"
+          required
+          tooltip="This description will be shown during the jobfair"
+          rules={CompanyProfileValidation.description}
+          name="description"
+        >
+          <TextArea autoSize={{ minRows: 5 }} showCount maxLength={3000} placeholder="Registration description" />
+        </Form.Item>
+        <PickJobPositionFormContainer form={form}/>
+      </Form>
+    </>
+
   )
 }
 
