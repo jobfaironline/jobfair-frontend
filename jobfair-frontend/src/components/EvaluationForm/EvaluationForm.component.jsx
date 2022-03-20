fdf21af5f6bfa4e6b8a6b710cb8e944a03d517ed
CaoSon1909 <phamcaoson1999@gmail.com>
2022-03-20 15:19:34 +0700

    CAP-279 Cv Registration detail page + evaluate registration

--- jobfair-frontend/src/components/EvaluationForm/EvaluationForm.component.jsx
+++ jobfair-frontend/src/components/EvaluationForm/EvaluationForm.component.jsx
@@ -4,51 +4,50 @@ import TextArea from "antd/es/input/TextArea";
 import {EvaluateConst} from "../../constants/JobPositionConst";
 
 const EvaluationFormComponent = ({onFinish, id, name}) => {
-    const [form] = Form.useForm();
-    return (
-        <>
-            <Form
-                form={form}
-                onFinish={onFinish}
-                requiredMark="required"
-                autoComplete="off"
-                scrollToFirstError={{block: "center", behavior: "smooth"}}
-            >
-                <Space direction="vertical" size="medium">
-                    <Form.Item name={name} noStyle
-                               initialValue={id}>
-                        <Input type="text" type="hidden"/>
-                    </Form.Item>
-                    <Form.Item
-                        label="Message"
-                        name={'message'}
-                        hasFeedback
-                    >
-                        <TextArea placeholder="Message"/>
-                    </Form.Item>
-
-                    <Space direction="horizontal">
-                        {EvaluateConst.map(item => (
-                            <div key={item.id}>
-                                <Form.Item name='status' noStyle>
-                                    <Input type="text" type="hidden"/>
-                                </Form.Item>
-                                <Button value={item.id} type="primary" htmlType="submit"
-                                    onClick={() => {
-                                        form.setFieldsValue({
-                                            ['status'] : item.id
-                                        })
-                                    }}
-                                >
-                                    {item.name}
-                                </Button>
-                            </div>
-                        ))}
-                    </Space>
-                </Space>
-            </Form>
-        </>
-    );
+  const [form] = Form.useForm();
+  return (
+    <>
+      <Form
+        form={form}
+        onFinish={onFinish}
+        requiredMark="required"
+        autoComplete="off"
+        scrollToFirstError={{block: "center", behavior: "smooth"}}
+      >
+        <Space direction="vertical" size="medium">
+          <Form.Item name={name} noStyle
+                     initialValue={id}>
+            <Input type="text" type="hidden"/>
+          </Form.Item>
+          <Form.Item
+            label="Message"
+            name={'message'}
+            hasFeedback
+          >
+            <TextArea placeholder="Message" showCount maxLength="1000"/>
+          </Form.Item>
+          <Space direction="horizontal">
+            {EvaluateConst.map(item => (
+              <div key={item.id}>
+                <Form.Item name='status' noStyle>
+                  <Input type="text" type="hidden"/>
+                </Form.Item>
+                <Button value={item.id} type="primary" htmlType="submit"
+                        onClick={() => {
+                          form.setFieldsValue({
+                            ['status']: item.id
+                          })
+                        }}
+                >
+                  {item.name}
+                </Button>
+              </div>
+            ))}
+          </Space>
+        </Space>
+      </Form>
+    </>
+  );
 };
 
 
