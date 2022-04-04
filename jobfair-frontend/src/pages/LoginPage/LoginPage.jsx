843f9f341723c9a185afeab682ffec27fbcc4093
CaoSon1909 <phamcaoson1999@gmail.com>
2022-04-04 10:49:39 +0700

    CAP-302 continue to rename folder (element -> MeshElement, navbar -> Navbar, login -> Login)

--- jobfair-frontend/src/pages/LoginPage/LoginPage.jsx
+++ jobfair-frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { Typography } from 'antd'
 import React, { useState } from 'react'
 import { injectStyle } from 'react-toastify/dist/inject-style'
-import LoginContainer from '../../containers/login/Login.container'
+import LoginContainer from '../../containers/Login/Login.container'
 import './LoginPage.styles.scss'
 
 if (typeof window !== 'undefined') {
