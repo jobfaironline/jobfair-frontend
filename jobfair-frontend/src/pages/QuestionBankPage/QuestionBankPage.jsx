00c216b6180532a378ccf43115bb2ea72c4fb8f5
Tien T. Truong <tientt1938@gmail.com>
2022-04-26 11:09:47 +0700

    [CAP-353] - finish create question

--- jobfair-frontend/src/pages/QuestionBankPage/QuestionBankPage.jsx
+++ jobfair-frontend/src/pages/QuestionBankPage/QuestionBankPage.jsx
@@ -1,18 +1,29 @@
-import './QuestionBank.styles.scss';
+import './QuestionBankPage.styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Typography } from 'antd';
-import { useLocation } from 'react-router-dom';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { useHistory, useLocation } from 'react-router-dom';
 import QuestionBankContainer from '../../containers/QuestionBank/QuestionBank.container';
 import React from 'react';
 
 const QuestionBankPage = () => {
-  //useLocation
   const location = useLocation();
   const jobPositionId = location.state?.jobPositionId;
+  const history = useHistory();
+
   return (
-    <div className='page'>
+    <div className='page question-bank-page'>
       <div className={'page-title'}>
         <Typography.Title level={2}>Question bank management</Typography.Title>
       </div>
+      <a
+        className={'back'}
+        onClick={() => {
+          history.goBack();
+        }}>
+        <FontAwesomeIcon icon={faArrowLeft} size={'1x'} color={'black'} />
+        <Typography.Title level={5}>Return to company profile page</Typography.Title>
+      </a>
       <QuestionBankContainer jobPositionId={jobPositionId} />
     </div>
   );
