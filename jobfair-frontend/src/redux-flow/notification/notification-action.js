adc4082296fbb949b79ae8003256f2a661b9e1fa
Tien T. Truong <tientt1938@gmail.com>
2022-05-02 16:27:28 +0700

    [CAP-371] - fix eslint

--- jobfair-frontend/src/redux-flow/notification/notification-action.js
+++ jobfair-frontend/src/redux-flow/notification/notification-action.js
@@ -1,10 +1,7 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {getNotification} from "../../services/jobhub-api/NotifcationControllerService";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getNotification } from '../../services/jobhub-api/NotifcationControllerService';
 
-export const fetchNotification = createAsyncThunk(
-  'notification/fetchNotification',
-  async () => {
-    const res =  await getNotification();
-    return res.data;
-  }
-);
+export const fetchNotification = createAsyncThunk('notification/fetchNotification', async () => {
+  const res = await getNotification();
+  return res.data;
+});
