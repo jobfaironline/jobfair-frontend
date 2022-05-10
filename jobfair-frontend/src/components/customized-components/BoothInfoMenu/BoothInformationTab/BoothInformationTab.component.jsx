1e20b4ef2bb92396a4925c45616c4edf0ed665be
CaoSon1909 <phamcaoson1999@gmail.com>
2022-05-10 11:44:36 +0700

    [CAP-386] - restyle attendant join booth screen

--- jobfair-frontend/src/components/customized-components/BoothInfoMenu/BoothInformationTab/BoothInformationTab.component.jsx
+++ jobfair-frontend/src/components/customized-components/BoothInfoMenu/BoothInformationTab/BoothInformationTab.component.jsx
@@ -1,8 +1,18 @@
+import './BoothInformationTab.styles.scss';
+import { Avatar, Typography } from 'antd';
+
 export const CompanyInformation = (props) => {
   const { data } = props;
   return (
-    <>
-      <p>{data.companyDescription}</p>
-    </>
+    <div className='tab-container'>
+      <Avatar shape='square' size={100} src={data.companyLogoURL} />
+      <Typography.Title level={3}>{data.name}</Typography.Title>
+      <Typography.Title level={4}>Company information</Typography.Title>
+      <Typography.Paragraph></Typography.Paragraph>
+      <Typography.Title level={4}>Company bio</Typography.Title>
+      <Typography.Paragraph>
+        <blockquote>{data.companyDescription}</blockquote>
+      </Typography.Paragraph>
+    </div>
   );
 };
