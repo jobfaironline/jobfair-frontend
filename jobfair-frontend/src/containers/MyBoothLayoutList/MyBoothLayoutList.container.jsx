e97db4b60748cb8e81e843c350ef00786e617fd7
ANH_KTV <70008423+ZERO-94@users.noreply.github.com>
2022-06-05 16:24:43 +0700

    update upload video

--- jobfair-frontend/src/containers/MyBoothLayoutList/MyBoothLayoutList.container.jsx
+++ jobfair-frontend/src/containers/MyBoothLayoutList/MyBoothLayoutList.container.jsx
@@ -46,19 +46,17 @@ const MyBoothLayoutListContainer = ({ myLayoutVisibility, setMyLayoutVisibility 
   return (
     <Modal visible={myLayoutVisibility} title='Title' onOk={handleOk} onCancel={handleCancel} width='100vw'>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-        {fakeData.map((layout) => {
-          return (
-            <Card
-              hoverable
-              style={{
-                width: 240,
-                marginBottom: '2rem'
-              }}
-              cover={<img alt='example' src={layout?.imgUrl} />}>
-              <Meta title={layout.name} />
-            </Card>
-          );
-        })}
+        {fakeData.map((layout) => (
+          <Card
+            hoverable
+            style={{
+              width: 240,
+              marginBottom: '2rem'
+            }}
+            cover={<img alt='example' src={layout?.imgUrl} />}>
+            <Meta title={layout.name} />
+          </Card>
+        ))}
       </div>
     </Modal>
   );
