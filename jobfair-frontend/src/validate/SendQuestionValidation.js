fae24e1e79a923a4cfdf8c253cf0b908979292bb
Tien T. Truong <tientt1938@gmail.com>
2022-06-14 23:34:56 +0700

    [CAP-420] - add Footer to Router

--- jobfair-frontend/src/validate/SendQuestionValidation.js
+++
@@ -1,6 +0,0 @@
-import { EMAIL_VALIDATOR, MAX_LENGTH_VALIDATOR, REQUIRED_VALIDATOR } from './GeneralValidation';
-
-export const SendQuestionValidation = {
-  email: [REQUIRED_VALIDATOR('Email'), ...EMAIL_VALIDATOR()],
-  question: [REQUIRED_VALIDATOR('Câu hỏi'), MAX_LENGTH_VALIDATOR('Câu hỏi', 1000)]
-};
