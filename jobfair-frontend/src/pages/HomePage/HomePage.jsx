36400eb57187c875474ac39a363dd71a2cf7b672
Tien T. Truong <tientt1938@gmail.com>
2022-06-15 19:48:16 +0700

    [CAP-420] - resolve request

--- jobfair-frontend/src/pages/HomePage/HomePage.jsx
+++ jobfair-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,37 +1,19 @@
-import { ATTENDANT, COMPANY_EMPLOYEE, COMPANY_MANAGER } from '../../constants/RoleType';
 import { FAQComponent } from '../../components/customized-components/FAQ/FAQ.components';
 import { HomePageAdComponent } from '../../components/customized-components/HomePage/HomePageAd.component';
-import { useSelector } from 'react-redux';
 import HomePageHeaderComponent from '../../components/customized-components/HomePage/HomePageHeader.component';
 import React from 'react';
 
-const HomePage = () => {
-  const role = useSelector((state) => state.authentication.user.roles);
-
-  const handleDisplayHomePage = (role) => {
-    switch (role) {
-      case COMPANY_MANAGER:
-      case COMPANY_EMPLOYEE:
-      case ATTENDANT:
-        return (
-          <div className='home'>
-            <h2>Home page</h2>
-          </div>
-        );
-      default:
-        return (
-          <div>
-            <HomePageHeaderComponent />
-            <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
-            <HomePageAdComponent />
-            <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
-            <div style={{ padding: '2% 10%' }}>
-              <FAQComponent />
-            </div>
-          </div>
-        );
-    }
-  };
-  return <div className='page fullscreen-page non-sub-nav-bar'>{handleDisplayHomePage(role)}</div>;
-};
+const HomePage = () => (
+  <div className='page fullscreen-page non-sub-nav-bar'>
+    <div>
+      <HomePageHeaderComponent />
+      <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
+      <HomePageAdComponent />
+      <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
+      <div style={{ padding: '2% 10%' }}>
+        <FAQComponent />
+      </div>
+    </div>
+  </div>
+);
 export default HomePage;
