c728064ed6d0eff9b727e61fc71d98c764416070
ZERO-94 <kimanh.tvka@gmail.com>
2022-06-15 19:53:19 +0700

    update push back logic

--- jobfair-frontend/src/pages/HomePage/HomePage.jsx
+++ jobfair-frontend/src/pages/HomePage/HomePage.jsx
@@ -2,18 +2,31 @@ import { FAQComponent } from '../../components/customized-components/FAQ/FAQ.com
 import { HomePageAdComponent } from '../../components/customized-components/HomePage/HomePageAd.component';
 import HomePageHeaderComponent from '../../components/customized-components/HomePage/HomePageHeader.component';
 import React from 'react';
+import { PATH_COMPANY_EMPLOYEE, PATH_COMPANY_MANAGER } from '../../constants/Paths/Path';
+import RoleType from '../../constants/RoleType';
 
-const HomePage = () => (
-  <div className='page fullscreen-page non-sub-nav-bar'>
-    <div>
-      <HomePageHeaderComponent />
-      <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
-      <HomePageAdComponent />
-      <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
-      <div style={{ padding: '2% 10%' }}>
-        <FAQComponent />
+const HomePage = () => {
+  switch (role) {
+    case RoleType.COMPANY_EMPLOYEE:
+      history.push(PATH_COMPANY_EMPLOYEE.JOB_FAIR_ASSIGNMENT_PAGE);
+      return;
+    case RoleType.COMPANY_MANAGER:
+      history.push(PATH_COMPANY_MANAGER.COMPANY_PROFILE_PAGE);
+      return;
+  }
+
+  return (
+    <div className='page fullscreen-page non-sub-nav-bar'>
+      <div>
+        <HomePageHeaderComponent />
+        <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
+        <HomePageAdComponent />
+        <div style={{ width: '100%', height: '10rem', backgroundColor: '#E5EAFB' }} />
+        <div style={{ padding: '2% 10%' }}>
+          <FAQComponent />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default HomePage;
