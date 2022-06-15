8459e76f659bc97d6bb9ad57fc9e5e888f948654
Tien T. Truong <tientt1938@gmail.com>
2022-06-15 21:55:04 +0700

    [DEMO] - fix constraint for shift when organizing a job fair

--- jobfair-frontend/src/constants/JobFairConst.js
+++ jobfair-frontend/src/constants/JobFairConst.js
@@ -94,6 +94,6 @@ export const JOB_FAIR_STATUS = {
 };
 
 export const SHIFT_DISABLE_TIME = {
-  MORNING_SHIFT: [0, 1, 2, 3, 4, 5, 6, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-  AFTERNOON_SHIFT: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 23]
+  MORNING_SHIFT: [0, 1, 2, 3, 4, 5, 6, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+  AFTERNOON_SHIFT: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 22, 23]
 };
