7b37d018ca2d7b3bb8c3ba79ca83a33284bf7635
CaoSon1909 <phamcaoson1999@gmail.com>
2022-06-17 08:57:52 +0700

    [FIX_BUG] - fix minor bugs

--- jobfair-frontend/src/components/customized-components/DragAndDropResume/RemoveResume.component.jsx
+++ jobfair-frontend/src/components/customized-components/DragAndDropResume/RemoveResume.component.jsx
@@ -1,5 +1,6 @@
-import { Divider, Typography } from 'antd';
+import { Descriptions, Divider, Tag, Typography } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { convertEnumToString } from '../../../utils/common';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
@@ -12,10 +13,20 @@ export const RemoveResumeComponent = (props) => {
       <Divider style={{ margin: '1rem 0' }} />
       <div style={{ display: 'flex', flexDirection: 'row' }}>
         <div>
-          <Text strong style={{ fontSize: '1.5rem' }}>
-            {' '}
-            {`CV: ${selectedResume.name} `}
-          </Text>
+          <Descriptions title='CV detail' bordered={false} column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}>
+            <Descriptions.Item label={<Text strong>Name </Text>}>{selectedResume?.name}</Descriptions.Item>
+            <Descriptions.Item label={<Text strong>Skills </Text>}>
+              {selectedResume?.skills.map((item) => (
+                <Tag color='blue'>{item.name}</Tag>
+              ))}
+            </Descriptions.Item>
+            <Descriptions.Item label={<Text strong>Job level </Text>}>
+              <Tag color='blue'>{convertEnumToString(selectedResume?.jobLevel)}</Tag>
+            </Descriptions.Item>
+            <Descriptions.Item label={<Text strong>Job title </Text>}>
+              <Tag color='blue'>{selectedResume?.jobTitle}</Tag>
+            </Descriptions.Item>
+          </Descriptions>
         </div>
         <div
           className={'remove-resume'}
