c26532568d50b156622712cd17f1df40b494b902
ZERO-94 <kimanh.tvka@gmail.com>
2022-06-29 22:25:28 +0700

    fix bug

--- jobfair-frontend/src/pages/ResumeDetailPage/ResumeDetailPage.jsx
+++ jobfair-frontend/src/pages/ResumeDetailPage/ResumeDetailPage.jsx
@@ -3,7 +3,6 @@ import { PageHeader } from 'antd';
 import { useHistory, useLocation } from 'react-router-dom';
 import React from 'react';
 import ResumeDetailForCompanyContainer from '../../containers/Resume/company/ResumeDetailForCompany.container';
-import PageLayoutWrapper from '../../components/commons/PageLayoutWrapper/PageLayoutWrapper.component';
 
 const ResumeDetailPage = () => {
   const history = useHistory();
@@ -11,10 +10,10 @@ const ResumeDetailPage = () => {
   const { resumeId } = location.state;
 
   return (
-    <PageLayoutWrapper className='page'>
+    <div className='page'>
       <PageHeader className='site-page-header' onBack={() => history.goBack()} title='Resume Detail Page' />
       <ResumeDetailForCompanyContainer resumeId={resumeId} />
-    </PageLayoutWrapper>
+    </div>
   );
 };
 export default ResumeDetailPage;
