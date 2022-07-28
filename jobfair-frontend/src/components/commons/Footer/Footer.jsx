80bb60a0bf9dc0ecb54761c3eb7a08c8867cbf1f
Tien T. Truong <tientt1938@gmail.com>
2022-07-28 16:36:43 +0700

    [TIEN] - fix footer

--- jobfair-frontend/src/components/commons/Footer/Footer.jsx
+++ jobfair-frontend/src/components/commons/Footer/Footer.jsx
@@ -1,56 +1,11 @@
 import './Footer.styles.scss';
-import { Button, Input } from 'antd';
-import { FacebookOutlined, GithubOutlined } from '@ant-design/icons';
 import React from 'react';
-
-const { TextArea } = Input;
 
 const Footer = () => (
   <div className='footer'>
-    <div className='left-side'>
-      <div className='logo'>
-        <img style={{ width: '40%' }} src={`${window.location.origin}/logo/logo_with_text.svg`} />
-      </div>
-      <div>©2022 JobHub, Inc.</div>
-      <div className='logo-click'>
-        <div className='children'>
-          <FacebookOutlined
-            style={{ fontSize: '2rem' }}
-            onClick={() => {
-              window.location.href = 'https://www.facebook.com/TrongKhanh.Kieu';
-            }}
-          />
-          <span className='description'>Facebook</span>
-        </div>
-        <div className='children'>
-          <GithubOutlined
-            style={{ fontSize: '2rem' }}
-            onClick={() => {
-              window.location.href = 'https://github.com/jobfaironline';
-            }}
-          />
-          <span className='description'>Git Hub</span>
-        </div>
-      </div>
-    </div>
-    <div className='right-side'>
-      <div className='form-contact'>
-        <div className='title'>Have a question? Send us an email</div>
-        <form action='mailto:tientt1938@gmail.com' method='post' encType='text/plain'>
-          <TextArea
-            style={{ marginTop: '0.5rem' }}
-            type='text'
-            name='comment'
-            className={'input'}
-            maxLength={500}
-            autoSize={{ minRows: 3, maxRows: 3 }}
-          />
-          <br />
-          <Button type='primary' htmlType='submit' style={{ marginTop: '0.5rem' }}>
-            Send
-          </Button>
-        </form>
-      </div>
+    <div className='logo'>
+      <img src={`${window.location.origin}/logo/logo_with_text.svg`} width='40%' />
+      <div style={{ marginTop: '2px' }}>©2022 JobHub, Inc.</div>
     </div>
   </div>
 );
