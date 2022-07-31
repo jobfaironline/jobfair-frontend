bfb12f552923d065a526c3cb2b8d673855fc72aa
Tien T. Truong <tientt1938@gmail.com>
2022-08-01 06:02:46 +0700

    [TIEN] - fix interview schedule algorithm

--- jobfair-frontend/src/components/customized-components/Resume/ResumeDetail.component.jsx
+++ jobfair-frontend/src/components/customized-components/Resume/ResumeDetail.component.jsx
@@ -5,26 +5,29 @@ import { ResumeOverview } from './Content/ResumeOverview.component';
 import { ResumeReferenceList } from './Content/ResumeReferenceList.component';
 import { ResumeSkillList } from './Content/ResumeSkillList.component';
 import { ResumeWorkHistoryList } from './Content/ResumeWorkHistoryList.component';
+import { useSelector } from 'react-redux';
 import React from 'react';
 import ResumeEducationList from './Content/ResumeEducationList.component';
+import RoleType from '../../../constants/RoleType';
 
 const { Title, Text } = Typography;
 
 export const ResumeDetailComponent = (props) => {
   const { data, isForCompany = false } = props;
   const isSuitable = data.matchingPoint * 100 > 50;
+  const role = useSelector((state) => state.authentication.user.roles);
+
+  const matchingStatement = () => {
+    if (role !== RoleType.COMPANY_EMPLOYEE) return null;
+    if (isSuitable) return <Text style={{ color: 'green' }}>This application is suitable for your job position</Text>;
+    return <Text style={{ color: 'red' }}>This application is not suitable for your job position</Text>;
+  };
   return (
     <div className='resume-detail'>
       <Row gutter={15}>
         <Col span={7}>
           <ResumeOverview userOverview={data.overviewData} isForCompany={isForCompany} />
-          <div>
-            {isSuitable ? (
-              <Text style={{ color: 'green' }}>This application is suitable for your job position</Text>
-            ) : (
-              <Text style={{ color: 'red' }}>This application is not suitable for your job position</Text>
-            )}
-          </div>
+          <div>{matchingStatement()}</div>
         </Col>
         <Col span={17}>
           <Card style={{ borderRadius: '8px' }} className={'list'}>
